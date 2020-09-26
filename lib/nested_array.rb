3f788305563a0455939b3035b5913145d4ca266c
akam alam <arundelfootball23@gmail.com>
2020-09-26 22:01:46 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -49,8 +49,8 @@ def matrix_lookup(matrix, row, column)
   
   produce_storage_room =[
   
-CONVENTIONAL_PRODUCE,
-ORGANIC_PRODUCE
+CONVENTIONAL_PRODUCE.sort,
+ORGANIC_PRODUCE.sort
 ]
  
  produce_storage_room[1][0]
