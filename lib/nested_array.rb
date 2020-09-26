746bcac8249d4f2d1428cf0ba5c50a56da776438
akam alam <arundelfootball23@gmail.com>
2020-09-26 21:59:31 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -47,7 +47,13 @@ end
 
 def matrix_lookup(matrix, row, column)
   
+  produce_storage_room =[
   
+CONVENTIONAL_PRODUCE.sort,
+ORGANIC_PRODUCE.sort
+]
+ 
+ produce_storage_room[0][1]
   
 #   # Given any matrix (array of arrays), a row index and a column index, 
 #   # Return the matrix's content at that row and and column
