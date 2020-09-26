4f400446fbdfc58ed20e67c0ab89135f3a3a423a
akam alam <arundelfootball23@gmail.com>
2020-09-26 22:07:03 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -53,7 +53,7 @@ CONVENTIONAL_PRODUCE,
 ORGANIC_PRODUCE
 ]
  
- produce_storage_room[0][1]
+ produce_storage_room
   
 #   # Given any matrix (array of arrays), a row index and a column index, 
 #   # Return the matrix's content at that row and and column
